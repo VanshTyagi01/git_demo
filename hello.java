62bdc8e810cd0e8d8d02d25f0383597061ddc518
Vansh Tyagi <vanshtyagi011@outlook.com>
2026-02-16 15:52:00 +0530

    added addition function

--- hello.java
+++ hello.java
@@ -3,5 +3,8 @@
 public class hello {
     public static void main(String args[]){
         System.out.println("Hello Jenkins from Java App");
+        int a = 10;
+        int b = 11;
+        System.out.println("Roll No "+(a+b));
     }
 }
